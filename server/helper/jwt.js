9e5f682423a89e4dbba88c0298ed4756bb125cdf
anto <irianto223@gmail.com>
2017-09-21 04:50:30 +0700

    auth pakai facebook

--- server/helper/jwt.js
+++ server/helper/jwt.js
@@ -1,10 +1,24 @@
 var jwt = require('jsonwebtoken');
 require('dotenv').config()
+const FB = require('fb');
+const fb = new FB.Facebook({version: 'v2.10'});
 
 const islogIn = (req, res, next) => {
+
   var decoded = jwt.verify(req.headers.token, process.env.SECRET, (err, decoded)=>{
     if(err){
-      res.send("anda belum login")
+      FB.api( '/me', 'get', { access_token: req.headers.fbaccesstoken }, response => {
+        if (!response.error) {
+          // console.log('ga error');
+          // console.log(response);
+          next()
+        }
+        else {
+          // console.log('error');
+          // console.log(response);
+          res.send("anda belum login")
+        }
+      });
     }else {
       req.id = decoded.id
       next()
